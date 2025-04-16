25cc9d4ac519a9450ada689439b4e3fdadcf2ba5
jdanos07 <jdanos07@gmail.com>
2025-04-16 05:10:04 -0400

    Deployment ready

--- Automotive_Shop_app/application/__init__.py
+++ Automotive_Shop_app/application/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
-from application.models import db
-from application.extensions import ma, limiter, cache
-from application.blueprints.customer import customers_bp
-from application.blueprints.mechanic import mechanics_bp
-from application.blueprints.service_ticket import service_tickets_bp
-from application.blueprints.vehicle import vehicles_bp
-from application.blueprints.inventory import inventory_bp
+from .models import db
+from .extensions import ma, limiter, cache
+from .blueprints.customer import customers_bp
+from .blueprints.mechanic import mechanics_bp
+from .blueprints.service_ticket import service_tickets_bp
+from .blueprints.vehicle import vehicles_bp
+from .blueprints.inventory import inventory_bp
 from flask_swagger_ui import get_swaggerui_blueprint
 
 SWAGGER_URL = '/api/docs'
